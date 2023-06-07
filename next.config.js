77321641f5c07266a20adab895f51f277a33de08
Matthew Glasser <matthewsglasser@gmail.com>
2023-06-06 21:50:56 -0400

    alumni page

--- next.config.js
+++ next.config.js
@@ -13,43 +13,11 @@ module.exports = {
         protocol: 'https',
         hostname: 'cdn.jaybots.org',
         port: '',
-        pathname: '/bot/2023-snow2-crop.jpeg',
       },
       {
         protocol: 'https',
-        hostname: 'cdn.jaybots.org',
+        hostname: 'jaybots.org',
         port: '',
-        pathname: '/bot/p-2023.jpeg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.jaybots.org',
-        port: '',
-        pathname: '/bot/2022.jpeg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.jaybots.org',
-        port: '',
-        pathname: '/bot/b-2022.jpeg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.jaybots.org',
-        port: '',
-        pathname: '/bot/2021.jpg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.jaybots.org',
-        port: '',
-        pathname: '/bot/2020.jpg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.jaybots.org',
-        port: '',
-        pathname: '/bot/2019.jpg',
       },
     ]
   }
