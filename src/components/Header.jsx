11315ab9800d56fc611ba2b6c8222c5baf5e2077
Matthew Glasser <matthewsglasser@gmail.com>
2023-11-04 17:10:52 -0400

    fixed header parallax (hopefully for good)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,31 @@ import Image from "next/image"
 
 export function Header({title, beforeBold, bold, afterBold}){
   return (
-    <div className='[transform-style:preserve-3d] [transform:translateZ(-20px)scale(3)] absolute w-screen h-screen lg:-left-36 [pointer-events:none] z-50'>
-      <div className="sm:flex items-center w-screen py-6 px-12 sm:py-12 lg:py-24 text-white text-center bg-gray-900 sm:bg-transparent bg-gradient-to-b from-gray-800 via-gray-700 via-60% z-50">
-        <div className='lg:hidden flex justify-center'>
-          <Image
-            src='https://cdn.jaybots.org/logo/logo.png'
-            width={250}
-            height={250}
-            className='rounded-full h-32 w-32 sm:h-auto sm:w-auto'
-            alt='logo'
-          />
-        </div>
-        <div className='w-full space-y-2 lg:space-y-4'>
-          <h1 className='text-4xl xl:text-7xl lg:text-6xl md:text-5xl font-extrabold'>{title}</h1>
-          <div className='flex w-full text-md md:text-lg lg:text-xl xl:text-2xl text-center justify-center space-x-2'>
-            <p className='font-thin'>
-              {beforeBold}<b className='font-semibold'>
-                {bold}</b>
-              {afterBold}
-            </p>
+    // <div className="[perspective:10px] [perspective-origin:top] w-full h-screen">
+      // <div className='[transform-style:preserve-3d] [transform:translateZ(-20px)scale(3)] -left-8 origin-top-right absolute w-full z-50'>
+        <div className="[transform-style:preserve-3d] [transform:translateZ(-20px)scale(3)] -left-8 lg:pl-72 origin-top-right absolute sm:flex items-center w-full py-6 px-12 sm:py-12 lg:py-24 text-white text-center bg-gray-900 sm:bg-transparent bg-gradient-to-b from-gray-800 via-gray-700 via-60% z-50">
+          <div className='lg:hidden flex justify-center'>
+            <Image
+              src='https://cdn.jaybots.org/logo/logo.png'
+              width={250}
+              height={250}
+              className='rounded-full h-32 w-32 sm:h-auto sm:w-auto'
+              alt='logo'
+            />
+          </div>
+          <div className='w-full space-y-2 lg:space-y-4'>
+            <h1 className='text-4xl xl:text-7xl lg:text-6xl md:text-5xl font-extrabold'>{title}</h1>
+            <div className='flex w-full text-md md:text-lg lg:text-xl xl:text-2xl text-center justify-center space-x-2'>
+              <p className='font-thin'>
+                {beforeBold}<b className='font-semibold'>
+                  {bold}</b>
+                {afterBold}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      // </div>
+    // </div>
   )
 }
 
