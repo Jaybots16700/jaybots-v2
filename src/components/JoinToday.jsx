e6aa9bd68a8c36eadcecc4a89342bf7ed979606e
Matthew Glasser <matthewsglasser@gmail.com>
2024-01-06 09:33:23 -0500

    brightened join now

--- src/components/JoinToday.jsx
+++ src/components/JoinToday.jsx
@@ -17,7 +17,7 @@ export function JoinToday() {
     <div className="py-6">
       <div className="relative isolate">
         <div className="mx-auto max-w-7xl sm:px-6 xl:px-8">
-          <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 xl:mx-0 xl:max-w-none xl:flex-row xl:items-center xl:py-20 xl:gap-x-20 xl:px-20">
+          <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/10 px-6 py-16 ring-1 ring-white/20 sm:rounded-3xl sm:p-8 xl:mx-0 xl:max-w-none xl:flex-row xl:items-center xl:py-20 xl:gap-x-20 xl:px-20">
             <Image
               className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl xl:aspect-square xl:h-auto xl:max-w-sm"
               src="https://file.coffee/u/9eBeiWCI4QcCuo.jpg"
