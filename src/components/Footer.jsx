4209b9fdc67258bc02354702e8060c4b5c7b2f6c
Matthew Glasser <matthewsglasser@gmail.com>
2024-01-12 10:21:56 -0500

    removed sponsor section of footer - temporary

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,7 +40,7 @@ export function Footer() {
             ))}
           </div>
         </div>
-        <div className='flex flex-col items-center border-t border-gray-400 pt-4 pb-2 md:justify-between'>
+        {/* <div className='flex flex-col items-center border-t border-gray-400 pt-4 pb-2 md:justify-between'>
           <p className='text-md text-gray-400'>Meet our <Link href='https://jaybotsboosters.org/sponsors' className='text-gray-200 font-semibold hover:font-bold'>Sponsors</Link></p>
           <div className='grid grid-cols-3 sm:flex space-x-8'>
             {Sponsers.map((item) => (
@@ -54,7 +54,7 @@ export function Footer() {
               </Link>
             ))}
           </div>
-        </div>
+        </div> */}
         <div className="flex flex-col items-center border-t border-gray-400 py-6 md:flex-row-reverse md:justify-between md:pt-6">
           <form className="flex w-full justify-center md:w-auto">
             <TextField
