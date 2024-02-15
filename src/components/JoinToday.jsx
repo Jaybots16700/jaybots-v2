b127dfe735aa4a9cbbca28067ca14ff5c77a7b99
matthewglasser <matthewsglasser@gmail.com>
2024-02-15 11:55:53 -0500

    update with corning images/vids

--- src/components/JoinToday.jsx
+++ src/components/JoinToday.jsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 import Colors from './Colors'
 
 const benefits = [
-  "Thrive in Competition",
+  'Thrive in Competition',
   'Develop Cutting-edge Skills',
   'Foster Lifelong Friendships',
   'Unlock Future Opportunities',
   'Embrace Team Collaboration',
-  "Experience Unforgettable Fun!",
+  'Experience Unforgettable Fun!',
 ]
 
 export function JoinToday() {
@@ -17,7 +17,7 @@ export function JoinToday() {
     <div className="py-6">
       <div className="relative isolate">
         <div className="mx-auto max-w-7xl sm:px-6 xl:px-8">
-          <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/10 px-6 py-16 ring-1 ring-white/20 sm:rounded-3xl sm:p-8 xl:mx-0 xl:max-w-none xl:flex-row xl:items-center xl:py-20 xl:gap-x-20 xl:px-20">
+          <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/10 px-6 py-16 ring-1 ring-white/20 sm:rounded-3xl sm:p-8 xl:mx-0 xl:max-w-none xl:flex-row xl:items-center xl:gap-x-20 xl:px-20 xl:py-20">
             <Image
               className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl xl:aspect-square xl:h-auto xl:max-w-sm"
               src="/images/Team.jpg"
@@ -26,9 +26,14 @@ export function JoinToday() {
               height={384}
             />
             <div className="w-full flex-auto">
-              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Join the team</h2>
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Join the team
+              </h2>
               <p className="mt-6 text-lg leading-8 text-gray-300">
-                Unleash your potential and join the Jaybots for an exhilarating journey of innovation, collaboration, and personal growth, where creativity thrives, skills sharpen, and lifelong friendships flourish amidst the exciting realm of technology and robotics.
+                Unleash your potential and join the Jaybots for an exhilarating
+                journey of innovation, collaboration, and personal growth, where
+                creativity thrives, skills sharpen, and lifelong friendships
+                flourish amidst the exciting realm of technology and robotics.
               </p>
               <ul
                 role="list"
@@ -36,14 +41,24 @@ export function JoinToday() {
               >
                 {benefits.map((benefit) => (
                   <li key={benefit} className="flex gap-x-3">
-                    <CheckCircleIcon className="h-7 w-5 flex-none" aria-hidden="true" />
+                    <CheckCircleIcon
+                      className="h-7 w-5 flex-none"
+                      aria-hidden="true"
+                    />
                     {benefit}
                   </li>
                 ))}
               </ul>
               <div className="mt-10 flex">
-                <Link href="https://interest.jaybots.org" target="_blank" className="group text-sm font-semibold leading-6 text-indigo-400 hover:font-bold">
-                  Join Today <span aria-hidden="true" className=''>&rarr;</span>
+                <Link
+                  href="https://interest.jaybots.org"
+                  target="_blank"
+                  className="group text-sm font-semibold leading-6 text-indigo-400 hover:font-bold"
+                >
+                  Join Today{' '}
+                  <span aria-hidden="true" className="">
+                    &rarr;
+                  </span>
                 </Link>
               </div>
             </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,9 +16,7 @@ import {
   faXTwitter,
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons'
-import {
-  faCommentsDollar,
-  } from '@fortawesome/free-solid-svg-icons'
+import { faCommentsDollar } from '@fortawesome/free-solid-svg-icons'
 import { linkStyle, allImages } from '@/config.jsx'
 
 import Colors from '@/components/Colors'
@@ -26,43 +24,41 @@ import { useEffect, useRef, useState } from 'react'
 
 const detectScroll = (ref, setVisible) => {
   const observer = new IntersectionObserver((entries) => {
-    const entry = entries[0];
-    setVisible(entry.isIntersecting);
+    const entry = entries[0]
+    setVisible(entry.isIntersecting)
   })
-  observer.observe(ref.current);
+  observer.observe(ref.current)
 }
 
-
 export default function Home() {
-
-  const [sponsorBtnVis, setSponsorBtnVis] = useState();
-  const sponsorBtnRef = useRef();
+  const [sponsorBtnVis, setSponsorBtnVis] = useState()
+  const sponsorBtnRef = useRef()
   useEffect(() => {
-    detectScroll(sponsorBtnRef, setSponsorBtnVis);
+    detectScroll(sponsorBtnRef, setSponsorBtnVis)
   }, [])
 
-  const [ytBtnVis, setYtBtnVis] = useState();
-  const ytBtnRef = useRef();
+  const [ytBtnVis, setYtBtnVis] = useState()
+  const ytBtnRef = useRef()
   useEffect(() => {
-    detectScroll(ytBtnRef, setYtBtnVis);
+    detectScroll(ytBtnRef, setYtBtnVis)
   }, [])
 
-  const [instaBtnVis, setInstaBtnVis] = useState();
-  const instaBtnRef = useRef();
+  const [instaBtnVis, setInstaBtnVis] = useState()
+  const instaBtnRef = useRef()
   useEffect(() => {
-    detectScroll(instaBtnRef, setInstaBtnVis);
+    detectScroll(instaBtnRef, setInstaBtnVis)
   }, [])
 
-  const [xBtnVis, setXBtnVis] = useState();
-  const xBtnRef = useRef();
+  const [xBtnVis, setXBtnVis] = useState()
+  const xBtnRef = useRef()
   useEffect(() => {
-    detectScroll(xBtnRef, setXBtnVis);
+    detectScroll(xBtnRef, setXBtnVis)
   }, [])
 
-  const [mediaVis, setMediaVis] = useState();
-  const mediaRef = useRef();
+  const [mediaVis, setMediaVis] = useState()
+  const mediaRef = useRef()
   useEffect(() => {
-    detectScroll(mediaRef, setMediaVis);
+    detectScroll(mediaRef, setMediaVis)
   }, [])
 
   return (
@@ -72,77 +68,143 @@ export default function Home() {
       </Head>
       <Nav current="Home" />
       <main>
-      <div className='flex flex-col lg:pl-64 animate-all h-screen w-full overflow-x-hidden overflow-y-scroll bg-black scrollbar scrollbar-track-slate-900 scrollbar-thumb-blue-900 z-50'>
-          <Header title="John Jay Robotics Team" bold="Jaybots" afterBold=" - FTC Robotics Team #16700" />
+        <div className="animate-all z-50 flex h-screen w-full flex-col overflow-x-hidden overflow-y-scroll bg-black scrollbar scrollbar-track-slate-900 scrollbar-thumb-blue-900 lg:pl-64">
+          <Header
+            title="John Jay Robotics Team"
+            bold="Jaybots"
+            afterBold=" - FTC Robotics Team #16700"
+          />
 
-          <div className='w-full pb-12 lg:pb-24 text-gray-400'>
-
-            <div className='w-full text-center space-y-6 text-xl p-6 sm:px-12 flex flex-col'>
-              <h2 className='text-5xl xl:text-6xl font-bold text-gray-200'>Who are we?</h2>
-              <div className='w-full flex justify-center'>
-                <p className='max-w-4xl leading-8'>We are the Jaybots - This is our fifth year competing in <Link href="https://www.firstinspires.org/robotics/ftc/" className={linkStyle} target='_blank'>
-                  FTC
-                </Link>
-                . After making it to the regional competition three years in a row, we are aiming to make it even further next year. We have completed our 2023 Game Season: <Link href="https://www.youtube.com/watch?v=HsitvZ0JaDc" className={linkStyle} target='_blank'>
-                  Power Play
-                </Link>
-                , and are preparing for the 2024 Game Season, <Link href="https://www.youtube.com/watch?v=6e-5Uo1dRic" className={linkStyle} target='_blank'>
-                  Center Stage
-                </Link>
-                . Join us on our journey!
+          <div className="w-full pb-12 text-gray-400 lg:pb-24">
+            <div className="flex w-full flex-col space-y-6 p-6 text-center text-xl sm:px-12">
+              <h2 className="text-5xl font-bold text-gray-200 xl:text-6xl">
+                Who are we?
+              </h2>
+              <div className="flex w-full justify-center">
+                <p className="max-w-4xl leading-8">
+                  We are the Jaybots - This is our fifth year competing in{' '}
+                  <Link
+                    href="https://www.firstinspires.org/robotics/ftc/"
+                    className={linkStyle}
+                    target="_blank"
+                  >
+                    FTC
+                  </Link>
+                  . After making it to the regional competition four years in a
+                  row, we are aiming to make it even further this year. We have
+                  made it to Regionals again in the 2024 Game Season,{' '}
+                  <Link
+                    href="https://www.youtube.com/watch?v=6e-5Uo1dRic"
+                    className={linkStyle}
+                    target="_blank"
+                  >
+                    Center Stage
+                  </Link>
+                  . Join us on our journey!
                 </p>
               </div>
             </div>
             <JoinToday />
 
-            <div className='relative isolate grid sm:grid-cols-2 grid-cols-1 w-full p-6 sm:px-12 gap-4 text-center text-lg sm:text-sm font-semibold md:text-xl'>
-              <Link ref={instaBtnRef} href="https://www.instagram.com/johnjayroboticsclub/" className={clsx({"h-12 rounded-lg flex justify-center items-center hover:text-white hover:font-bold bg-gray-800 ring-1 ring-white/10 hover:ring-white/25 hover:scale-y-110 hover:scale-x-[103%] xl:hover:scale-x-[101%] duration-500 overflow-hidden group": true,
-                '-translate-x-64 opacity-0': !instaBtnVis
-              })} target='_blank'>
-                <div className='bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-400 justify-center items-center w-full h-full opacity-0 group-hover:opacity-100 duration-500 absolute'/>
-                <FontAwesomeIcon icon={faInstagram} className="h-8 mr-2 z-10" />
-                <p className='z-10'>@JohnJayRoboticsClub</p>
+            <div className="relative isolate grid w-full grid-cols-1 gap-4 p-6 text-center text-lg font-semibold sm:grid-cols-2 sm:px-12 sm:text-sm md:text-xl">
+              <Link
+                ref={instaBtnRef}
+                href="https://www.instagram.com/johnjayroboticsclub/"
+                className={clsx({
+                  'group flex h-12 items-center justify-center overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/10 duration-500 hover:scale-x-[103%] hover:scale-y-110 hover:font-bold hover:text-white hover:ring-white/25 xl:hover:scale-x-[101%]': true,
+                  '-translate-x-64 opacity-0': !instaBtnVis,
+                })}
+                target="_blank"
+              >
+                <div className="absolute h-full w-full items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-400 opacity-0 duration-500 group-hover:opacity-100" />
+                <FontAwesomeIcon icon={faInstagram} className="z-10 mr-2 h-8" />
+                <p className="z-10">@JohnJayRoboticsClub</p>
               </Link>
-              <Link ref={ytBtnRef} href="https://www.youtube.com/@jaybots16700" className={clsx({"h-12 rounded-lg flex justify-center items-center hover:text-white hover:font-bold bg-gray-800 ring-1 ring-white/10 hover:ring-white/25 hover:scale-y-110 hover:scale-x-[103%] xl:hover:scale-x-[101%] duration-500 hover:bg-red-600": true,
-                  "translate-x-64 opacity-0": !ytBtnVis
-              })} target='_blank'>
-                <FontAwesomeIcon icon={faYoutube} className="h-8 mr-2" />
+              <Link
+                ref={ytBtnRef}
+                href="https://www.youtube.com/@jaybots16700"
+                className={clsx({
+                  'flex h-12 items-center justify-center rounded-lg bg-gray-800 ring-1 ring-white/10 duration-500 hover:scale-x-[103%] hover:scale-y-110 hover:bg-red-600 hover:font-bold hover:text-white hover:ring-white/25 xl:hover:scale-x-[101%]': true,
+                  'translate-x-64 opacity-0': !ytBtnVis,
+                })}
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faYoutube} className="mr-2 h-8" />
                 <p>Jaybots #16700</p>
               </Link>
-              <Link ref={xBtnRef} href="https://twitter.com/RoboticsJay" className={clsx({"h-12 rounded-lg flex justify-center items-center hover:text-white hover:font-bold bg-gray-800 ring-1 ring-white/10 hover:ring-white/25 hover:scale-y-110 hover:scale-x-[103%] xl:hover:scale-x-[101%] duration-500 hover:bg-black": true,
-                'opacity-0 -translate-x-64': !xBtnVis
-              })} target='_blank'>
-                <FontAwesomeIcon icon={faXTwitter} className="h-8 mr-2" />
+              <Link
+                ref={xBtnRef}
+                href="https://twitter.com/RoboticsJay"
+                className={clsx({
+                  'flex h-12 items-center justify-center rounded-lg bg-gray-800 ring-1 ring-white/10 duration-500 hover:scale-x-[103%] hover:scale-y-110 hover:bg-black hover:font-bold hover:text-white hover:ring-white/25 xl:hover:scale-x-[101%]': true,
+                  '-translate-x-64 opacity-0': !xBtnVis,
+                })}
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faXTwitter} className="mr-2 h-8" />
                 <p>@RoboticsJay</p>
               </Link>
-              <Link ref={sponsorBtnRef} href="https://jaybotsboosters.org/sponsors" className={clsx({"h-12 rounded-lg flex justify-center items-center hover:text-white hover:font-bold bg-gray-800 ring-1 ring-white/10 hover:ring-white/25 hover:scale-y-110 hover:scale-x-[103%] xl:hover:scale-x-[101%] duration-500 hover:bg-blue-600": true,
-                  "translate-x-64 opacity-0": !sponsorBtnVis
-              })} target='_blank'>
-                <FontAwesomeIcon icon={faCommentsDollar} className="h-8 mr-2" />
+              <Link
+                ref={sponsorBtnRef}
+                href="https://jaybotsboosters.org/sponsors"
+                className={clsx({
+                  'flex h-12 items-center justify-center rounded-lg bg-gray-800 ring-1 ring-white/10 duration-500 hover:scale-x-[103%] hover:scale-y-110 hover:bg-blue-600 hover:font-bold hover:text-white hover:ring-white/25 xl:hover:scale-x-[101%]': true,
+                  'translate-x-64 opacity-0': !sponsorBtnVis,
+                })}
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faCommentsDollar} className="mr-2 h-8" />
                 <p>Our Sponsors!</p>
               </Link>
             </div>
 
-            <div ref={mediaRef} className='w-full sm:flex h-fit sm:h-[42rem] xl:h-[50rem] sm:space-x-12 space-y-8 relative isolate sm:justify-between'>
-
-              <div className={clsx({"w-full sm:w-min h-fit sm:h-full sm:flex items-center duration-500": true,
-                  "-translate-x-full": !mediaVis
-                })}>
-                <div className="h-fit bg-white/5 p-12 w-full sm:w-min sm:rounded-r-4xl ring-1 ring-white/10">
-                  <h2 className='text-gray-100 text-5xl sm:text-6xl md:text-7xl xl:text-8xl pb-3 font-bold w-fit pr-16 border-b-8 border-white md:mr-20'>Media</h2>
-                  <p className='text-gray-300 text-base md:text-lg font-light mt-4'>A visual tapestry that showcases the remarkable moments of triumph, camaraderie, and innovation captured through captivating photos, immortalizing the unwavering dedication, creativity, and teamwork that defines our exceptional robotics team.</p>
+            <div
+              ref={mediaRef}
+              className="relative isolate h-fit w-full space-y-8 sm:flex sm:h-[42rem] sm:justify-between sm:space-x-12 xl:h-[50rem]"
+            >
+              <div
+                className={clsx({
+                  'h-fit w-full items-center duration-500 sm:flex sm:h-full sm:w-min': true,
+                  '-translate-x-full': !mediaVis,
+                })}
+              >
+                <div className="h-fit w-full bg-white/5 p-12 ring-1 ring-white/10 sm:w-min sm:rounded-r-4xl">
+                  <h2 className="w-fit border-b-8 border-white pb-3 pr-16 text-5xl font-bold text-gray-100 sm:text-6xl md:mr-20 md:text-7xl xl:text-8xl">
+                    Media
+                  </h2>
+                  <p className="mt-4 text-base font-light text-gray-300 md:text-lg">
+                    A visual tapestry that showcases the remarkable moments of
+                    triumph, camaraderie, and innovation captured through
+                    captivating photos, immortalizing the unwavering dedication,
+                    creativity, and teamwork that defines our exceptional
+                    robotics team.
+                  </p>
                   <div className="mt-4 flex">
-                <Link href="/media" target="_blank" className="group text-sm font-semibold leading-6 text-indigo-400 hover:font-bold">
-                  View all Media <span aria-hidden="true" className=''>&rarr;</span>
-                </Link>
-              </div>
+                    <Link
+                      href="/media"
+                      target="_blank"
+                      className="group text-sm font-semibold leading-6 text-indigo-400 hover:font-bold"
+                    >
+                      View all Media{' '}
+                      <span aria-hidden="true" className="">
+                        &rarr;
+                      </span>
+                    </Link>
+                  </div>
                 </div>
               </div>
 
-              <div className={clsx({'relative pl-4 rounded-2xl sm:rounded-r-none overflow-hidden pr-4 sm:pr-8 sm:h-full h-[32rem] mx-8 sm:mx-0 duration-500': true,
-                  'translate-x-full': !mediaVis
-                })}>
-                <ReviewColumn reviews={allImages} msPerPixel={10} caption={false} />
+              <div
+                className={clsx({
+                  'relative mx-8 h-[32rem] overflow-hidden rounded-2xl pl-4 pr-4 duration-500 sm:mx-0 sm:h-full sm:rounded-r-none sm:pr-8': true,
+                  'translate-x-full': !mediaVis,
+                })}
+              >
+                <ReviewColumn
+                  reviews={allImages}
+                  msPerPixel={10}
+                  caption={false}
+                />
                 <div className="pointer-events-none absolute inset-x-0 top-0 h-16 bg-gradient-to-b from-gray-700" />
                 <div className="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-gray-700" />
               </div>
@@ -150,38 +212,75 @@ export default function Home() {
               <Colors />
             </div>
 
-            <div className='w-full px-12 mt-20'>
-              <video controls className='w-full'>
-                <source src='/videos/home.mp4' type="video/mp4" />
-                Video unavailable.
-              </video>
+            <div className="mt-20 aspect-video w-full px-12">
+              <iframe
+                className=" h-full w-full"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/IkMCntCRsLE?si=jN2frnBaT_ovuywc&amp;clip=UgkxQFFDhOqml9jQqJzNyPtE9tClnhFLQ7JG&amp;clipt=EMGgsgcYh46zBw"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
             </div>
 
-            <div id="contact" className='relative w-full text-center space-y-4 sm:text-xl mt-8 font-light px-4 text:base'>
-              <h2 className='text-4xl font-bold text-gray-200'>Contact Us!</h2>
-              <p>Like what you see? <Link href="https://interest.jaybots.org" className={linkStyle} target='_blank'>
+            <div
+              id="contact"
+              className="text:base relative mt-8 w-full space-y-4 px-4 text-center font-light sm:text-xl"
+            >
+              <h2 className="text-4xl font-bold text-gray-200">Contact Us!</h2>
+              <p>
+                Like what you see?{' '}
+                <Link
+                  href="https://interest.jaybots.org"
+                  className={linkStyle}
+                  target="_blank"
+                >
                   Join Today!
-                </Link> Feel free to contact us at <Link href="mailto:interest@jaybots.org?subject=Question" className={linkStyle} target='_blank'>
+                </Link>{' '}
+                Feel free to contact us at{' '}
+                <Link
+                  href="mailto:interest@jaybots.org?subject=Question"
+                  className={linkStyle}
+                  target="_blank"
+                >
                   interest@jaybots.org
                 </Link>
               </p>
-              <p>Interested in becoming a <Link href="https://jaybotsboosters.org/sponsor" className={linkStyle} target='_blank'>
+              <p>
+                Interested in becoming a{' '}
+                <Link
+                  href="https://jaybotsboosters.org/sponsor"
+                  className={linkStyle}
+                  target="_blank"
+                >
                   sponsor
                 </Link>
-                ? You can reach us at <Link href="mailto:joe@jaybots.org?subject=Sponsorship Question" className={linkStyle} target='_blank'>
+                ? You can reach us at{' '}
+                <Link
+                  href="mailto:joe@jaybots.org?subject=Sponsorship Question"
+                  className={linkStyle}
+                  target="_blank"
+                >
                   joe@jaybots.org
                 </Link>
               </p>
-              <p>If there&apos;s anything else that you would like to discuss, send us an email at <Link href="mailto:hello@jaybots.org" className={linkStyle} target='_blank'>
+              <p>
+                If there&apos;s anything else that you would like to discuss,
+                send us an email at{' '}
+                <Link
+                  href="mailto:hello@jaybots.org"
+                  className={linkStyle}
+                  target="_blank"
+                >
                   hello@jaybots.org
                 </Link>
               </p>
             </div>
-
           </div>
 
           <Footer />
-
         </div>
       </main>
     </>
