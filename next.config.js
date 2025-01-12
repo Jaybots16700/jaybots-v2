a9d7396f9f445ceebda4dcf6ff8b7cd2fec7d29f
matthewglasser <matthewsglasser@gmail.com>
2025-01-12 13:11:22 -0500

    false

--- next.config.js
+++ next.config.js
@@ -162,7 +162,7 @@ module.exports = {
       {
         source: '/live',
         destination: 'https://excelsiorny.org/live',
-        permanent: true,
+        permanent: false,
       },
     ]
   },
