ae6e28e536c3ceb914aabe4be146f29012bf3640
Veda652 <vedasrikodak@gmail.com>
2025-01-19 03:02:28 +0000

    SEASON logo

--- src/config.jsx
+++ src/config.jsx
@@ -480,7 +480,7 @@ export const games = [
   {
     name: 'Into The Deep',
     year: '2024-2025',
-    logo: 'JAYBOTSSS.psd.png',
+    logo: 'https://drive.google.com/uc?export=view&id=10pqamkKEG_t-zy5qyLZCJyybZNvKY0e-',
     images: '',
     ,// TODO: @Veda652 Add logo
     logowidth: 500,
